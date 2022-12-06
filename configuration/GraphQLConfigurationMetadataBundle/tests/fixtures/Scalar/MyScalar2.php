a0882d93b1e37fefe2d6de0144bc4a9be7eeca54
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-12-06 15:51:04 +0300

    Fix merge conflict

--- configuration/GraphQLConfigurationMetadataBundle/tests/fixtures/Scalar/MyScalar2.php
+++ configuration/GraphQLConfigurationMetadataBundle/tests/fixtures/Scalar/MyScalar2.php
@@ -12,6 +12,6 @@ use Overblog\GraphQL\Bundle\ConfigurationMetadataBundle\Metadata as GQL;
  */
 #[GQL\Scalar(name: "MyScalar", scalarType: "newObject('App\Type\EmailType')")]
 #[GQL\Scalar(name: "MyScalar2", scalarType: "newObject('App\Type\EmailType')")]
-class MyScalar2
+final class MyScalar2
 {
 }
