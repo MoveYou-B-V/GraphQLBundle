a3737fe69c613e8cf0d6833da96a6ef5ef3e07c7
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-12-06 15:58:54 +0300

    Fix merge conflict

--- configuration/GraphQLConfigurationMetadataBundle/tests/fixtures/Input/Planet.php
+++ configuration/GraphQLConfigurationMetadataBundle/tests/fixtures/Input/Planet.php
@@ -13,25 +13,25 @@ use Overblog\GraphQL\Bundle\ConfigurationMetadataBundle\Metadata as GQL;
  */
 #[GQL\Input]
 #[GQL\Description("Planet Input type description")]
-class Planet
+final class Planet
 {
     /**
      * @GQL\InputField(type="String!", defaultValue="Sun")
      */
     #[GQL\InputField(type: "String!")]
-    protected string $name = 'Sun';
+    public string $name = 'Sun';
 
     /**
      * @GQL\InputField(type="Int!")
      */
     #[GQL\InputField(type: "Int!")]
-    protected string $population;
+    public string $population;
 
     /**
      * @GQL\InputField
      */
     #[GQL\InputField]
-    protected string $description;
+    public string $description;
 
     /**
      * @GQL\InputField
@@ -39,22 +39,22 @@ class Planet
      */
     #[GQL\InputField]
     // @phpstan-ignore-next-line
-    protected $diameter;
+    public ?int $diameter;
 
     /**
      * @GQL\InputField
      * @ORM\Column(type="boolean")
      */
     #[GQL\InputField]
-    protected int $variable;
+    public int $variable;
 
     // @phpstan-ignore-next-line
-    protected $dummy;
+    public $dummy;
 
     /**
      * @GQL\InputField(defaultValue={})
      * @ORM\Column(type="text[]")
      */
     #[GQL\InputField]
-    protected array $tags = [];
+    public array $tags = [];
 }
