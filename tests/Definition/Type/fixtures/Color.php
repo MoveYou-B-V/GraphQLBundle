bf82afb8fe07e164d3ca845d6611a85e2c1a6158
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-12-09 21:53:55 +0300

    Remove outdated code

--- tests/Definition/Type/fixtures/Color.php
+++ tests/Definition/Type/fixtures/Color.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\Definition\Type\fixtures;
 
-use Overblog\GraphQL\Bundle\ConfigurationMetadataBundle\Metadata as GQL;
-
 /**
- * @GQL\Enum
- * @GQL\EnumValue(name="RED", description="The color red")
+ * @requires PHP 8.1
  */
-#[GQL\Enum]
 enum Color
 {
-    #[GQL\Description('The color red')]
     case RED;
 
     case GREEN;
